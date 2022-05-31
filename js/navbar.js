772e8da7f5a6a469e3115c6d47ba58dbffa6f19e
Joe <56809242+Jo3-L@users.noreply.github.com>
2022-05-31 09:51:41 -0700

    navbar: add toggler for responsiveness

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,11 @@
-(() => {
-    fetch('/partials/navbar.html')
-        .then((resp) => resp.text())
-        .then((body) => {
-            document.getElementById('nav-placeholder').innerHTML = body;
-        });
-})();
+fetch('/partials/navbar.html')
+	.then((resp) => resp.text())
+	.then((body) => {
+		document.getElementById('nav-placeholder').innerHTML = body;
+
+		const collapseButton = document.getElementById('navbar-collapse-button');
+		collapseButton.addEventListener('click', () => {
+			const nav = document.getElementById('vshacks-nav');
+			nav.classList.toggle('bg-dark');
+		});
+	});
