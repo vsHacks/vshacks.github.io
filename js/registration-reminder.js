22e41d4932ee3e546072ef25923e364ac7594ff3
Joe <56809242+Jo3-L@users.noreply.github.com>
2022-06-03 18:32:06 -0700

    homepage: add registration reminder

---
+++ js/registration-reminder.js
@@ -0,0 +1,6 @@
+const REGISTRATION_END_DATE = new Date(2022, 5, 11, 59, 59);
+
+if (new Date() < REGISTRATION_END_DATE) {
+	const registrationReminder = new bootstrap.Toast(document.getElementById('registration-reminder'));
+	registrationReminder.show();
+}
