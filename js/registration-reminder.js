fabe203955866fab5535d9c9a62dc4292648ab1e
Joe <56809242+Jo3-L@users.noreply.github.com>
2022-06-03 18:48:58 -0700

    homepage: delay showing registration reminder

--- js/registration-reminder.js
+++ js/registration-reminder.js
@@ -2,5 +2,5 @@ const REGISTRATION_END_DATE = new Date(2022, 5, 11, 59, 59);
 
 if (new Date() < REGISTRATION_END_DATE) {
 	const registrationReminder = new bootstrap.Toast(document.getElementById('registration-reminder'));
-	registrationReminder.show();
+	setTimeout(() => registrationReminder.show(), 5_000);
 }
