fe40e8b119cabffd883cb06bb24d1525f128792b
Andrei <andreisva2023@gmail.com>
2023-05-19 20:36:35 -0700

    remove border around navbar items

--- src/lib/navbar-item.svelte
+++ src/lib/navbar-item.svelte
@@ -11,17 +11,12 @@
         color: black;
         text-align: center;
         line-height: 50px;
-        padding: 0 30px;
         text-decoration: none;
-        transition: 0.1s;
         margin: 0 5px;
-        border-radius: 10px;
-        box-shadow: 0 0 0 2px grey;
     }
 
     .navbar-item:hover {
-        border-width: 5px;
-        box-shadow: 0 0 0 3px grey;
+        text-decoration: underline;
     }
 
 </style>
