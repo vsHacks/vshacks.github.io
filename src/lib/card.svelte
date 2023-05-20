af6438775d98cc7d3bf93d4b3036c88fe8a69998
Andrei <andreisva2023@gmail.com>
2023-05-20 00:05:36 -0700

    add fade-in animation to cards

--- src/lib/card.svelte
+++ src/lib/card.svelte
@@ -1,14 +1,24 @@
 <script>
     export let title;
     export let image;
+
+    let visible = false;
+
+    function reveal(e) {
+        const observer = new IntersectionObserver(entries => {
+            visible = entries[0].isIntersecting;
+        });
+
+        observer.observe(e);
+    }
 </script>
 
-<div class="card">
+<div class="card" class:visible={visible} use:reveal>
     <h1 class="card-title">{title}</h1>
     <div class="card-content">
         <p><slot/></p>
         {#if image != null}
-        <img class="card-image" src={image}>
+        <img class="card-image" src={image} alt="">
         {/if}
     </div>
 </div>
@@ -20,6 +30,13 @@
         max-width: 600px;
         margin-bottom: 30px;
         box-shadow: -1px 5px 10px lightgrey;
+        opacity: 0;
+        transition: 0.8s;
+    }
+
+    .visible {
+        opacity: 1.0;
+        top: 0px;
     }
 
     .card-image {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,14 +3,17 @@
     import Container from '$lib/container.svelte';
     import Card from '$lib/card.svelte';
 
-    import people from '$lib/assets/undraw_people.svg'
+    import people from '$lib/assets/undraw_people.svg';
+    import knowledge from '$lib/assets/undraw_knowledge.svg';
 </script>
-
 
 <Banner/>
 <Container>
     <Card title="Meet New People" image={people}>
         At vsHacks, you will have the unique opportunity to work with like-minded students from all across the Greater Vancouver area. Build a team of up to four people and start hacking!
+    </Card>
+    <Card title="Learn New Things" image={knowledge}>
+        Regardless of your skill level, we're certain that you'll learn something new at vsHacks from our experienced mentors. From web development to Discord bots, our workshops have something for everyone. Check out our schedule for further details.
     </Card>
     <h1>Welcome to SvelteKit</h1>
     <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
